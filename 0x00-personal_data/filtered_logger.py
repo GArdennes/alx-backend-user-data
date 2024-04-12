9374562ca2a149f611703b7fc69c2da3405bfc10
GArdennes <thekevin.afachao@gmail.com>
2024-04-12 12:06:26 +0000

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,18 +16,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Returns a connector to the database.
     """
-   db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-   db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-   db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-   db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-   connection = mysql.connector.connect(
-           host=db_host,
-           port=3306,
-           user=db_user,
-           password=db_pwd,
-           database=db_name,
-           )
-   return connection
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+            host=db_host,
+            port=3306,
+            user=db_user,
+            password=db_pwd,
+            database=db_name,
+            )
+    return connection
 
 
 def filter_datum(
@@ -86,7 +86,7 @@ def main():
     Main implementation
     """
     fields = ""
-    columns = fields.splilt(,)
+    columns = fields.splilt(",")
     query = "SELECT {} FROM users;".format(fields)
     info_logger = get_logger()
     connection = get_db()
@@ -110,4 +110,3 @@ def main():
                     )
             log_record = logging.LogRecord(*args)
             info_logger.handle(log_record)
-
